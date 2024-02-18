4b2da12622cda63c8996c0371e2745bc84034e33
lijinlong <2693175852@qq.com>
2024-02-18 20:38:03 +0800

    fix:调整过滤接口

--- admin/src/main/java/com/oxygen/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/oxygen/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,12 +1,14 @@
 package com.oxygen.shortlink.admin.common.biz.user;
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author LiJinLong
@@ -19,15 +21,23 @@ public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
 
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
+            "/api/short-link/v1/user/login",
+            "/api/short-link/v1/user/has-username"
+    );
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String username = httpServletRequest.getHeader("username");
-        String token = httpServletRequest.getHeader("token");
-        Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-        if (userInfoJsonStr != null) {
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)) {
+            String username = httpServletRequest.getHeader("username");
+            String token = httpServletRequest.getHeader("token");
+            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+            if (userInfoJsonStr != null) {
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
