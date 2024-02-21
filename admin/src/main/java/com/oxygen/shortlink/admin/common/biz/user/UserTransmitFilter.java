86b341366ca2c6c7c1084e29f90e2c57f645ccbb
lijinlong <2693175852@qq.com>
2024-02-21 19:25:38 +0800

    feature:完善登录拦截器提供友好提示

--- admin/src/main/java/com/oxygen/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/oxygen/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,14 +1,22 @@
 package com.oxygen.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
+import com.oxygen.shortlink.admin.common.convention.exception.ClientException;
+import com.oxygen.shortlink.admin.common.convention.result.Results;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
+
+import static com.oxygen.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * @author LiJinLong
@@ -22,8 +30,8 @@ public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
 
     private static final List<String> IGNORE_URI = Lists.newArrayList(
-            "/api/short-link/v1/user/login",
-            "/api/short-link/v1/user/has-username"
+            "/api/short-link/amdin/v1/user/login",
+            "/api/short-link/amdin/v1/user/has-username"
     );
 
     @Override
@@ -31,10 +39,23 @@ public class UserTransmitFilter implements Filter {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URI.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/short-link/amdin/v1/user/") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                String json = JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL)));
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse)servletResponse, json);
+                }
+                Object userInfoJsonStr = null;
+                try {
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        returnJson((HttpServletResponse)servletResponse, json);
+                    }
+                } catch (Exception e) {
+                    returnJson((HttpServletResponse)servletResponse, json);
+                }
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
@@ -46,5 +67,11 @@ public class UserTransmitFilter implements Filter {
         }
     }
 
-
+    private void returnJson(HttpServletResponse response, String json) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        }
+    }
 }
