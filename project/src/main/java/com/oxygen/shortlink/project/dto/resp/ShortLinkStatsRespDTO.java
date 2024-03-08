48d91f7490efc3a8373c530514b51a9777a19734
lijinlong <2693175852@qq.com>
2024-03-08 21:11:38 +0800

    feature: 开发单个短链接按时间区间返回聚合监控数据，包含（访问曲线、访问前10地区、24小时分布、高频访问IP、一周分布、操作系统记录、浏览器记录、访客类型、访问网络、访问设备）

--- project/src/main/java/com/oxygen/shortlink/project/dto/resp/ShortLinkStatsRespDTO.java
+++ project/src/main/java/com/oxygen/shortlink/project/dto/resp/ShortLinkStatsRespDTO.java
@@ -1,6 +1,9 @@
 package com.oxygen.shortlink.project.dto.resp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -11,6 +14,9 @@ import java.util.List;
  * @date 1.0
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ShortLinkStatsRespDTO {
 
     /**
